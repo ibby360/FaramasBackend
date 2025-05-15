cad3f1113a296ea7f9fe5c7907d93ecfcd782cbe
Ibrahim Ramadhan <ibrahimramadhan360@outlook.com>
2025-05-15 23:19:37 +0300

    Deleted aold files

--- custom-addons/README.md
+++
@@ -1,1 +0,0 @@
-## Add Custom Project Modules in here
--- custom-addons/openapi/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
-from . import main
-from . import api
-from . import pinguin
